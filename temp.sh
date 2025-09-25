20bfb7447eb54b82eaf777854d59b1cdec6f1189
ShiShiMaru81 <paulanindya2001@gmail.com>
2025-09-25 10:57:51 +0600

    Folder and Files have been created & ready to go

---
+++ temp.sh
@@ -0,0 +1,22 @@
+mkdir -p src
+mkdir -p research
+
+
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+
+touch .env
+touch setup.py
+
+touch app.py
+
+touch README.md
+
+touch research/trail.ipynb
+
+touch requirements.txt
+
+touch testing.py
+
+echo "Created project structure with necessary files and directories."
